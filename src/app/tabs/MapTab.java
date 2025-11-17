e0a631de4c5e1eeebefdb1a434e6c0ff551d854a
sim <human.automata2567@gmail.com>
2025-11-16 19:01:23 -0800

    WIP: MapTab

--- src/app/tabs/MapTab.java
+++ src/app/tabs/MapTab.java
@@ -1,23 +1,29 @@
 package app.tabs;
 
-import javax.swing.*;
 import app.Role;
+import java.awt.*;
+import java.io.File;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 
 public class MapTab extends JPanel {
 
-    public MapTab(Role role) {
+  public MapTab(Role role) {
 
-        add(new JLabel("Map"));
+    setLayout(new BorderLayout());
+    JButton importMap = new JButton("importMap");
+    JButton noMapFound = new JButton("No map found :(");
 
-        //if (role == Role.DM) {
-        //    add(new JLabel("NPC character sheet"));
-        //    add(new JButton("Create a new NPC"));
-        //} else {
-        //    add(new JLabel("Your Character"));
-        //    add(new JButton("Yay"));
-        //}
+    try {
+      // Load the image
+      Image img = ImageIO.read(new File("./state/baseMap.png"));
+      JLabel label = new JLabel(new ImageIcon(img));
+      add(label, BorderLayout.CENTER);
+    } catch (Exception e) {
+      add(noMapFound, BorderLayout.CENTER);
+      e.printStackTrace();
     }
+
+    add(importMap, BorderLayout.SOUTH);
+  }
 }
-
-
-
--- src/app/tabs/NotesTab.java
+++ src/app/tabs/NotesTab.java
@@ -1,33 +1,30 @@
 package app.tabs;
 
+import app.logic.Notes;
 import java.awt.*;
 import java.nio.file.*;
 import javax.swing.*;
-import app.Role;
-import app.logic.Notes;
-
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
 
 public class NotesTab extends JPanel {
 
-    //public Notes note;
+  public NotesTab() {
 
-    public NotesTab() {
-        //note.load();
-        Notes note = new Notes();
+    Notes note = new Notes();
 
-        setLayout(new BorderLayout());
-        JTextArea textArea = new JTextArea("Insert text here!");
-        textArea.setText(note.load());
-        JScrollPane textContainer = new JScrollPane(textArea);
-        textArea.setFont(new Font("Monospaced", Font.BOLD, 18));
-        textContainer.setBorder(BorderFactory.createEmptyBorder(40, 40, 40, 40));
-        textArea.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, 2, true));
-        add(textContainer, BorderLayout.CENTER);
+    setLayout(new BorderLayout());
 
-        JButton save = new JButton("Save");
-        save.addActionListener(e -> note.save(textArea.getText()));
-        add(save, BorderLayout.SOUTH);
-    }
+    JTextArea textArea = new JTextArea("Insert text here!");
+    textArea.setText(note.load());
+    JScrollPane textContainer = new JScrollPane(textArea);
+    textArea.setFont(new Font("Monospaced", Font.BOLD, 18));
+
+    textContainer.setBorder(BorderFactory.createEmptyBorder(40, 40, 40, 40));
+    textArea.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, 2, true));
+
+    add(textContainer, BorderLayout.CENTER);
+
+    JButton save = new JButton("Save");
+    save.addActionListener(e -> note.save(textArea.getText()));
+    add(save, BorderLayout.SOUTH);
+  }
 }
