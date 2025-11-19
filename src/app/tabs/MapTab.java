4e68ec9c5d98debde073d8ae9c5c7ac72eedc31d
Tyler Fairleigh <tylerfairleigh@outlook.com>
2025-11-19 11:26:40 -0800

    Let the refactoring begin!

--- src/app/tabs/MapTab.java
+++ src/app/tabs/MapTab.java
@@ -10,12 +10,13 @@ import javax.swing.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.awt.event.MouseAdapter;
+import javax.swing.JLayeredPane;
 
 public class MapTab extends JPanel {
 
   private boolean showGrid = false;
   private BufferedImage img;
-  private JPanel panel;
+  private JLayeredPane panel;
 
   // @Override
   // void mouseMoved(MouseEvent e) {
@@ -29,54 +30,36 @@ public class MapTab extends JPanel {
 
     setLayout(new BorderLayout());
 
+    JLayeredPane jLayeredPane = new JLayeredPane();
+
     try {
       img = ImageIO.read(new File("./state/baseMap.png"));
 
-      JLayeredPane poiLayer = new JLayeredPane();
-      poiLayer.setPreferredSize(new Dimension(img.getWidth(), img.getHeight()));
-
       // draw the image + optional grid
-      panel =
-          new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-              super.paintComponent(g);
+      panel = new JLayeredPane() {
+        @Override
+        protected void paintComponent(Graphics g) {
+          super.paintComponent(g);
+          g.drawImage(img, (getWidth()  - img.getWidth())  / 2, (getHeight() - img.getHeight()) / 2, null);
+        }
 
-              g.drawImage(img, (getWidth()  - img.getWidth())  / 2, (getHeight() - img.getHeight()) / 2, null);
-
-              if (showGrid) {
-                g.setColor(new Color(255, 0, 0, 120));
-                int cell = 25;
-                for (int x = (getWidth() - img.getWidth())  / 2; x < (img.getWidth() + getWidth()) / 2; x += cell) 
-                  g.drawLine(x, (getHeight() - img.getHeight()) / 2, x, (img.getHeight() + getHeight()) / 2);
-                for (int y = (getHeight() - img.getHeight()) / 2; y < (img.getHeight() + getHeight()) / 2; y += cell) 
-                  g.drawLine( (getWidth() - img.getWidth())  / 2, y, (img.getWidth() + getWidth()) / 2, y);
-              }
-            }
-
-            // Scrooling
-            @Override
-            public Dimension getPreferredSize() {
-              return new Dimension(img.getWidth(), img.getHeight());
-            }
-          };
-
-      
-      JButton gridButton = new JButton("Toggle Grid");
-      gridButton.addActionListener(e -> toggleGrid());
+        // Scrooling
+        @Override
+        public Dimension getPreferredSize() {
+          return new Dimension(img.getWidth(), img.getHeight());
+        }
+      };
 
       JScrollPane scroll = new JScrollPane(panel);
       scroll.setBorder(BorderFactory.createEmptyBorder(40, 40, 40, 40));
 
       JButton importMap = new JButton("importMap");
 
-      add(importMap, BorderLayout.NORTH);
-      add(scroll, BorderLayout.CENTER);
+      //add(importMap, BorderLayout.NORTH);
+      jLayeredPane.add(scroll, JLayeredPane.DEFAULT_LAYER);
+      scroll.setVisible(true);
 
       if (role == Role.DM) {
-        add(gridButton, BorderLayout.SOUTH);
-        add(poiLayer);
-        addPOI(poiLayer);
       }
 
     } catch (Exception e) {
@@ -92,7 +75,6 @@ public class MapTab extends JPanel {
             JButton button = new JButton("Button");
             button.setBounds(e.getX(), e.getY(), 30, 30);
             add(button);
-            pane.setOpaque(false);
             pane.repaint();
           }
         });
