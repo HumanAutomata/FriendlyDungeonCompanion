3eee92f1ed8a455fdcbdbb90e51ec84291b58b03
sim <human.automata2567@gmail.com>
2025-11-18 20:51:47 -0800

    add togglable grid and prep Map class

---
+++ src/app/logic/Map.java
@@ -0,0 +1,17 @@
+package app.logic;
+
+import java.io.File;
+import javax.imageio.ImageIO;
+
+public class Map {
+
+    //final Path file;
+    //
+    //public BaseMap() {
+    //    // store in user home by default
+    //    this.file = Path.of("./state/notes.txt");
+    //}
+    //
+    //return imagepath ? buffered image
+
+}
--- src/app/tabs/MapTab.java
+++ src/app/tabs/MapTab.java
@@ -1,8 +1,8 @@
 package app.tabs;
 
 import app.Role;
+import app.logic.Map;
 import java.awt.*;
-import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
@@ -11,35 +11,62 @@ import javax.swing.*;
 public class MapTab extends JPanel {
 
   private boolean showGrid = false;
+  private BufferedImage img;
+  private JPanel panel;
 
-  public MapTab(Role role) {
+  public MapTab(Role role /*BufferedImage image*/ ) {
 
     setLayout(new BorderLayout());
 
     try {
-      // Load image into the container
-      BufferedImage img = ImageIO.read(new File("./state/baseMap.png"));
-      ImageIcon icon = new ImageIcon(img);
-      JLabel label = new JLabel(icon);
-      JScrollPane scroll = new JScrollPane(label);
+      img = ImageIO.read(new File("./state/baseMap.png"));
+
+      // draw the image + optional grid
+      panel =
+          new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+              super.paintComponent(g);
+
+              g.drawImage(img, 0, 0, null);
+
+              if (showGrid) {
+                g.setColor(new Color(255, 0, 0, 120));
+                int cell = 25;
+                for (int x = 0; x < img.getWidth(); x += cell) g.drawLine(x, 0, x, img.getHeight());
+                for (int y = 0; y < img.getHeight(); y += cell) g.drawLine(0, y, img.getWidth(), y);
+              }
+            }
+
+            // Scrooling
+            @Override
+            public Dimension getPreferredSize() {
+              return new Dimension(img.getWidth(), img.getHeight());
+            }
+          };
+
+      JButton gridButton = new JButton("Toggle Grid");
+      gridButton.addActionListener(e -> toggleGrid());
+
+      JScrollPane scroll = new JScrollPane(panel);
       scroll.setBorder(BorderFactory.createEmptyBorder(40, 40, 40, 40));
 
+      JButton importMap = new JButton("importMap");
+
+      add(importMap, BorderLayout.NORTH);
       add(scroll, BorderLayout.CENTER);
+
+      if (role == Role.DM) {
+        add(gridButton, BorderLayout.SOUTH);
+      }
 
     } catch (Exception e) {
       e.printStackTrace();
     }
   }
 
+  public void toggleGrid() {
+    showGrid = !showGrid;
+    panel.repaint();
+  }
 }
-
-// JButton importMap = new JButton("importMap");
-// JButton noMapFound = new JButton("No map found :(");
-
-// if (role == Role.DM) {
-//    add(new JLabel("NPC character sheet"));
-//    add(new JButton("Create a new NPC"));
-// } else {
-//    add(new JLabel("Your Character"));
-//    add(new JButton("Yay"));
-// }
