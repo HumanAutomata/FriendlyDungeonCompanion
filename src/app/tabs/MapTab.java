633ed78a5500c89aedf767902800ea817b2879f8
sim <human.automata2567@gmail.com>
2025-11-18 20:22:50 -0800

    fit map to container and allow scrool

--- src/app/tabs/MapTab.java
+++ src/app/tabs/MapTab.java
@@ -2,28 +2,44 @@ package app.tabs;
 
 import app.Role;
 import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class MapTab extends JPanel {
 
+  private boolean showGrid = false;
+
   public MapTab(Role role) {
 
     setLayout(new BorderLayout());
-    JButton importMap = new JButton("importMap");
-    JButton noMapFound = new JButton("No map found :(");
 
     try {
-      // Load the image
-      Image img = ImageIO.read(new File("./state/baseMap.png"));
-      JLabel label = new JLabel(new ImageIcon(img));
-      add(label, BorderLayout.CENTER);
+      // Load image into the container
+      BufferedImage img = ImageIO.read(new File("./state/baseMap.png"));
+      ImageIcon icon = new ImageIcon(img);
+      JLabel label = new JLabel(icon);
+      JScrollPane scroll = new JScrollPane(label);
+      scroll.setBorder(BorderFactory.createEmptyBorder(40, 40, 40, 40));
+
+      add(scroll, BorderLayout.CENTER);
+
     } catch (Exception e) {
-      add(noMapFound, BorderLayout.CENTER);
       e.printStackTrace();
     }
-
-    add(importMap, BorderLayout.SOUTH);
   }
+
 }
+
+// JButton importMap = new JButton("importMap");
+// JButton noMapFound = new JButton("No map found :(");
+
+// if (role == Role.DM) {
+//    add(new JLabel("NPC character sheet"));
+//    add(new JButton("Create a new NPC"));
+// } else {
+//    add(new JLabel("Your Character"));
+//    add(new JButton("Yay"));
+// }
