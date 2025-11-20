d8a3475603141cdac49db8c2cdc83c7f14dd883f
sim <human.automata2567@gmail.com>
2025-11-19 17:25:52 -0800

    Map: add popup for POI creation - change command to create POI to Ctrl + Click - create popup with title, desc, and image path fields - add cancel button to exit the popup

--- src/app/tabs/MapTab.java
+++ src/app/tabs/MapTab.java
@@ -17,9 +17,11 @@ public class MapTab extends JPanel {
   private JPanel PopupPanel;
   private int imgWidth;
   private int imgHeight;
+  private int popupWidth = 800;
+  private int popupHeight = 1000;
 
   public MapTab(Role role) {
-    setLayout(new BorderLayout());
+    setLayout(new BorderLayout()); // divide tab into  center and 4 quadrants
 
     try {
 
@@ -32,11 +34,13 @@ public class MapTab extends JPanel {
             @Override
             protected void paintComponent(Graphics g) {
               super.paintComponent(g);
+              // offset to draw in the middle
               int offsetWidth = (getWidth() - img.getWidth()) / 2;
               int offsetHeight = (getHeight() - img.getHeight()) / 2;
               g.drawImage(img, offsetWidth, offsetHeight, null);
             }
 
+            // resizing
             @Override
             public Dimension getPreferredSize() {
               return new Dimension(imgWidth, imgHeight);
@@ -45,14 +49,86 @@ public class MapTab extends JPanel {
       imagePanel.setOpaque(true);
       imagePanel.setBounds(0, 0, imgWidth, imgHeight);
 
-      // draw the POIs
+      // draw the POI panel
       POIPanel = new JPanel(null);
       POIPanel.setOpaque(false);
       POIPanel.setPreferredSize(new Dimension(imgWidth, imgHeight));
       POIPanel.setBounds(0, 0, imgWidth, imgHeight);
 
-      // draw the Popup
-      PopupPanel = new JPanel();
+      // draw the Popup panel
+      PopupPanel = new JPanel(new BorderLayout());
+      int offsetWidth = (imgWidth - popupWidth) / 2;
+      int offsetHeight = (imgHeight - popupHeight) / 2;
+      PopupPanel.setBounds(offsetWidth, offsetHeight, popupWidth, popupHeight);
+      PopupPanel.setBackground(new Color(255, 255, 255, 0));
+      PopupPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+      // title
+      JLabel popupTitle = new JLabel("Point of Interest");
+      popupTitle.setFont(popupTitle.getFont().deriveFont(Font.BOLD, 56f));
+      popupTitle.setHorizontalAlignment(SwingConstants.CENTER);
+      PopupPanel.add(popupTitle, BorderLayout.NORTH);
+
+      // put contents in a single row
+      JPanel content = new JPanel();
+      content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
+
+      JLabel title = new JLabel("Title");
+      title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+      JTextField titleField = new JTextField();
+      titleField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 28));
+
+      JLabel desc = new JLabel("Description");
+      desc.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+      JTextField descField = new JTextField();
+      descField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 28));
+
+      JLabel path = new JLabel("New Image Path (please use the full path!)");
+      path.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+      JTextField pathField = new JTextField();
+      pathField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 28));
+
+
+      // add all components to content
+      content.add(title);
+      content.add(titleField);
+      content.add(Box.createVerticalStrut(8));
+      content.add(desc);
+      content.add(descField);
+      content.add(Box.createVerticalStrut(8));
+      content.add(path);
+      content.add(pathField);
+
+      PopupPanel.add(content, BorderLayout.CENTER);
+
+      JPanel buttons = new JPanel(new BorderLayout());
+
+      JButton save = new JButton("Save");
+      JButton cancel = new JButton("Cancel");
+
+      save.addMouseListener(
+          new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+              System.out.println("saved!");
+            }
+          });
+      cancel.addMouseListener(
+          new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+              System.out.println("Deleted");
+              layerPane.remove(PopupPanel);
+              layerPane.repaint();
+            }
+          });
+      buttons.add(save, BorderLayout.WEST);
+      buttons.add(cancel, BorderLayout.EAST);
+
+      PopupPanel.add(buttons, BorderLayout.SOUTH);
 
       // add the Map, POIs, and Popup to the layer pane
       layerPane = new JLayeredPane();
@@ -62,7 +138,7 @@ public class MapTab extends JPanel {
 
       // make the layer pane scrollable (resizable) and add it to the tab
       JScrollPane scroll = new JScrollPane(layerPane);
-      scroll.setBorder(BorderFactory.createEmptyBorder(40, 40, 40, 40));
+      scroll.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
       add(scroll, BorderLayout.CENTER);
 
       // other buttons
@@ -79,7 +155,8 @@ public class MapTab extends JPanel {
               @Override
               public void mousePressed(MouseEvent e) {
 
-                if (SwingUtilities.isLeftMouseButton(e)) {
+                // Ctrl + Left Click to create a POI
+                if (e.isControlDown() && SwingUtilities.isLeftMouseButton(e)) {
 
                   // button
                   JButton b = new JButton("X");
@@ -97,7 +174,7 @@ public class MapTab extends JPanel {
                             POIPanel.remove(b);
                             POIPanel.repaint();
                           } else if (SwingUtilities.isLeftMouseButton(e)) {
-                            System.out.println("create popup");
+                            layerPane.add(PopupPanel, JLayeredPane.POPUP_LAYER);
                           }
                         }
                       });
